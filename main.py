8d8f586c7c9ace231e64068429a66de6570989be
nadav-ye <yeffet.nadav@gmail.com>
2024-12-15 17:35:26 +0200

    basic async implementation

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import address_approvals
 import approvals_dto
-
-from typing import List, Dict, Any
-
+import asyncio
+import logging
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import List, Dict, Any
 
 
 app = FastAPI()
+logger = logging.getLogger("uvicorn.error")
 
 
 class ApprovalsRequest(BaseModel):
@@ -15,14 +16,23 @@ class ApprovalsRequest(BaseModel):
     include_token_price: bool | None = None
 
 
+async def _fetch_async_approvals(owner: str, semaphore: asyncio.Semaphore, result: Dict[str, List[Dict[str, Any]]]):
+    async with semaphore:
+        result[owner] = await address_approvals.get_address_approvals(owner)
+
 @app.post("/get_approvals")
 async def get_approvals(request: ApprovalsRequest) -> Dict[str, Any]:
-    result = dict()
-    for address in set(request.addresses):
-        approvals: List[Dict] = await address_approvals.get_address_approvals(address)
+    results = dict()
+
+    semaphore = asyncio.Semaphore(6)
+    addresses = set(request.addresses)
+    tasks = [_fetch_async_approvals(address, semaphore, results) for address in addresses]
+
+    await asyncio.gather(*tasks)
+    response = dict()
+    for owner in results.keys():
         approvals_dtos = []
-        for approval in approvals:
+        for approval in results[owner]:
             approvals_dtos.append(approvals_dto.approval_dto_from_log(approval))
-        result[address] = approvals_dtos
-    print(result)
-    return result
+        response[owner] = approvals_dtos
+    return response
